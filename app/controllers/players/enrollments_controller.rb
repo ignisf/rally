4403bea3e1c28824b29b030038456c42ce1e606e
ignisf <bordjukov@gmail.com>
2024-05-07 21:03:02 +0300

    Make sure we don't time out the enrollment cookie

--- app/controllers/players/enrollments_controller.rb
+++ app/controllers/players/enrollments_controller.rb
@@ -2,7 +2,7 @@ class Players::EnrollmentsController < ApplicationController
   def create
     @team = Team.find_by!(code: params[:team_code])
 
-    cookies.signed[:my_team] = {value: @team.id, expires: 365.days}
+    cookies.signed[:my_team] = {value: @team.id, expires: 365.days.from_now, secure: true, httponly: true}
 
     redirect_to root_path # , notice: "You are now part of #{@team.name}!"
   end
